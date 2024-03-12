1ca46874872ead828baa9559e3f17a13acd29971
Abubakar1010 <abubakar850772@gmail.com>
2024-03-12 15:46:21 +0600

    project setup with react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className=' text-center text-xl text-white'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App

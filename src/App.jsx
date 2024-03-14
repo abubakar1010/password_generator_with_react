3f956714a84f85b5140a1c6b925ef91a36099c6a
Abubakar1010 <abubakar850772@gmail.com>
2024-03-14 19:50:20 +0600

    implement number and character in password

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,12 @@ function App() {
 
   },[length,isCharacterAdded,isNumberAdded,isNumberAdded])
 
+
+  const handleCopyToClipboard = () => {
+
+    
+  }
+
   
 
   return (
@@ -59,7 +65,7 @@ function App() {
 
       />
 
-      <button className='text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-sm px-8 py-2.5 text-center'>Copy</button>
+      <button className='text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-sm px-8 py-2.5 text-center' onClick={handleCopyToClipboard}>Copy</button>
 
     </div>
 
